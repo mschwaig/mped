b833784e35f448af8468f8b2f6274e9b8fcc3312
Martin Schwaighofer <meschti@gmail.com>
2016-04-18 19:36:06 +0200

    initial commit of mped implementation

---
+++ mped_cs/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mped_cs
+{
+    class Program
+    {
+
+        /// <summary>
+        /// This method returns an enumeration with all the possible strings that are produced
+        /// if we take the alphabet of characters that the string consists of, and map every character
+        /// of that alphabet to a character in a different alphabet in all possible ways.
+        /// 
+        /// For example if the string is "ab" and the alphabet is {'x', 'y'}, 
+        /// we see, that the source alphabet is {'a', 'b'} and the following four mappings are possible:
+        /// 1) a->x, b->x
+        /// 2) a->x, b->y
+        /// 3) a->y, b->x,
+        /// 4) a->y, b->y
+        /// If we apply those four possible mappings to the input string s, they produce the following four strings:
+        /// 1) "xx"
+        /// 2) "xy"
+        /// 3) "yx"
+        /// 4) "yy"
+        /// This list is what the method would return as an enueration.
+        /// </summary>
+        /// <param name="s">The string, which is mapped to an alphabet.</param>
+        /// <param name="destination_alphabet">The alphabet that the string is mapped to.</param>
+        /// <returns>An enumeration with all the possible mappings of a given string to a given alphabet.</returns>
+        public static IEnumerable<string> mappings(string s, char[] destination_alphabet)
+        {
+            char[] source_alphabet = s.Distinct().OrderBy(x => x).ToArray();
+
+            SortedDictionary<char, int> reverse_mapping = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < source_alphabet.Length; i++)
+            {
+                reverse_mapping.Add(source_alphabet[i], i);
+            }
+
+            int[] mapping = new int[source_alphabet.Length];
+
+            int pos;
+
+            while (true)
+            {
+                yield return String.Concat(s.Select(x => destination_alphabet[mapping[reverse_mapping[x]]]));
+
+                pos = source_alphabet.Length - 1;
+
+                while (pos >= 0 && mapping[pos] + 1 >= destination_alphabet.Length) {
+                    mapping[pos] = 0;
+                    pos = pos - 1;
+                }
+
+                if (pos < 0) yield break;
+                mapping[pos] = mapping[pos] + 1;
+            }
+        }
+
+        public static int ed(string a, string b) {
+            int[,] distance = new int[a.Length + 1, b.Length + 1];
+
+            for (int i = 0; i < a.Length + 1; i++)
+            {
+                distance[i, 0] = i;
+            }
+
+            for (int j = 0; j < b.Length + 1; j++)
+            {
+                distance[0, j] = j;
+            }
+
+            for (int j = 0; j < b.Length; j++)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a.ElementAt(i) == b.ElementAt(j))
+                    {
+                        distance[i + 1, j + 1] = distance[i, j];
+                    }
+                    else
+                    {
+                        int delete = distance[i, j + 1] + 1;
+                        int insert = distance[i + 1, j] + 1;
+                        int substitute = distance[i, j] + 1;
+                        distance[i + 1, j + 1] = Math.Min(delete, Math.Min(insert, substitute));
+                    }
+                }
+            }
+           
+            int result = distance[a.Length, b.Length];
+            return result;
+        }
+
+
+        public static int mped(string a, string b)
+        {
+            char[] alphabet_a = a.Distinct().OrderBy(x => x).ToArray();
+            char[] alphabet_b = b.Distinct().OrderBy(x => x).ToArray();
+
+            int minimal_ed = Int32.MaxValue;
+
+            foreach (string mapping in mappings(a, alphabet_b))
+            {
+                int edit_distance_for_mapping = ed(mapping, b);
+                if (edit_distance_for_mapping < minimal_ed)
+                {
+                    minimal_ed = edit_distance_for_mapping;
+                }
+            }
+
+            return minimal_ed;
+        }
+
+        static void Main(string[] args)
+        {
+            // computeMped();
+
+            IEnumerable<string> m = mappings("abc", new char[] {'x', 'y', 'z'});
+
+            mappingsTest();
+
+            edTest();
+
+            mpedTest();
+        }
+
+        /// <summary>
+        /// Tests for the mapping method.
+        /// </summary>
+        static void mappingsTest() {
+            List<String> mappings1x1 = mappings("a", new char[] { 'x'}).ToList();
+
+            Debug.Assert(mappings1x1.Contains("x"));
+            Debug.Assert(mappings1x1.Count == 1);
+
+            List<String> mappings1x2 = mappings("a", new char[] { 'x', 'y' }).ToList();
+
+            Debug.Assert(mappings1x2.Contains("x"));
+            Debug.Assert(mappings1x2.Contains("y"));
+            Debug.Assert(mappings1x2.Count == 2);
+
+            List<String> mappings2x1 = mappings("ab", new char[] {'x'}).ToList();
+
+            Debug.Assert(mappings2x1.Contains("xx"));
+            Debug.Assert(mappings2x1.Count == 1);
+
+            List<String> mappings2x2 = mappings("ab", new char[] { 'x', 'y' }).ToList();
+
+            Debug.Assert(mappings2x2.Contains("xx"));
+            Debug.Assert(mappings2x2.Contains("xy"));
+            Debug.Assert(mappings2x2.Contains("yx"));
+            Debug.Assert(mappings2x2.Contains("yy"));
+            Debug.Assert(mappings2x2.Count == 4);
+        }
+
+        /// <summary>
+        /// Tests for the ed method.
+        /// </summary>
+        static void edTest()
+        {
+            Debug.Assert(ed("a","a") == 0);
+            Debug.Assert(ed("a", "b") == 1);
+            Debug.Assert(ed("a", "") == 1);
+            Debug.Assert(ed("", "a") == 1);
+            Debug.Assert(ed("ab", "b") == 1);
+            Debug.Assert(ed("ab", "a") == 1);
+            Debug.Assert(ed("a", "ab") == 1);
+            Debug.Assert(ed("b", "ab") == 1);
+            Debug.Assert(ed("ab", "ab") == 0);
+            Debug.Assert(ed("aa", "ab") == 1);
+        }
+
+        /// <summary>
+        /// Tests for MPED method.
+        /// </summary>
+        static void mpedTest() {
+            Debug.Assert(mped("a", "x") == 0);
+            Debug.Assert(mped("ab","xy") == 0);
+            Debug.Assert(mped("aa", "xy") == 1);
+            Debug.Assert(mped("ab", "xx") == 0);
+            Debug.Assert(mped("abc", "abc") == 0);
+            Debug.Assert(mped("AAABCCDDCAC", "1102322033") == 4);
+            Debug.Assert(mped("AAABCCDDCAA", "2210500155") == 5);
+            Debug.Assert(mped("AAABCCDCADD", "BABAEFEAFAD") == 5); // todo: check this manually
+        }
+    }
+}
