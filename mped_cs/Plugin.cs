79c288785420035b010dd9f145cc10f0f4293d46
Martin Schwaighofer <meschti@gmail.com>
2016-06-03 19:22:34 +0200

    added HeuristicLab PluginInfrastructure assembly as a dependency, added code signing, changed version number of plugin to match HeuristicLab version number  and added Plugin.cs file - all accoring to the guide at http://dev.heuristiclab.com/trac.fcgi/wiki/Documentation/DevelopmentCenter/CreateNewPluginUsingVS

---
+++ mped_cs/Plugin.cs
@@ -0,0 +1,10 @@
+﻿using HeuristicLab.PluginInfrastructure;
+
+namespace HeuristicLab.GreatIdea.ThePlugin
+{
+    [Plugin("mped_cs", "Implementation of the Multi-parameterized Edit Distance", "3.3.13.0")]
+    [PluginFile("mped_cs.dll", PluginFileType.Assembly)]
+    public class Plugin : PluginBase
+    {
+    }
+}
