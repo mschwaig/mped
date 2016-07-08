37c789659e98cf9b4ba3087f93c533b522c84fe2
Martin Schwaighofer <meschti@gmail.com>
2016-07-08 20:42:53 +0200

    Added project for problem generation in JSON format.

---
+++ mped_problemgen/Program.cs
@@ -0,0 +1,40 @@
+﻿using at.mschwaig.mped.definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace at.mschwaig.mped.problemgen
+{
+    class Program
+    {
+        static List<Problem> generateComparisonData()
+        {
+
+            List<Problem> problems = new List<Problem>();
+            RandomNumberGenerator r = new RNGCryptoServiceProvider();
+
+            for (int alphabet_pow = 2; alphabet_pow <= 5; alphabet_pow++)
+                for (int length_pow = 4; length_pow <= 4; length_pow++)
+                    for (int similarity = 0; similarity <= 8; similarity++)
+                    {
+                        int alphabet_size = 1 << alphabet_pow;
+                        int string_length = 1 << (length_pow * 2);
+                        int related_char_count = string_length * similarity / 8;
+                        problems.Add(ProblemData.generateProblem(alphabet_size, string_length, related_char_count, r));
+                    }
+
+            return problems;
+        }
+
+        static void Main(string[] args)
+        {
+            var res = generateComparisonData();
+
+            var asdf = Newtonsoft.Json.JsonConvert.SerializeObject(res);
+            System.IO.File.WriteAllText("test.txt", asdf);
+        }
+    }
+}
---
+++ mped_problemgen/RandomNumberGeneratorExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+namespace at.mschwaig.mped.problemgen
+{
+    static class RandomNumberGeneratorExtensions
+    {
+        // Fisher-Yates shuffle as described here: http://stackoverflow.com/a/1262619/2066744
+        public static void Shuffle<T>(this RandomNumberGenerator rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next<RandomNumberGenerator>(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+
+        // convert random bytes to random int in range
+        public static int Next<T>(this RandomNumberGenerator rng, int max_number)
+        {
+            byte[] arr = new byte[4];
+            int result;
+            do
+            {
+                rng.GetBytes(arr);
+                arr[3] = (byte)(arr[3] & 0x7f); // eliminate sign bit
+                result = BitConverter.ToInt32(arr, 0) % max_number;
+            } while (result >= Int32.MaxValue - (Int32.MaxValue % max_number));
+
+            return result;
+        }
+    }
+}
