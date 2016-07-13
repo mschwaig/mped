54c763160173b6f33659683bf689d2f8f7c4fd04
Martin Schwaighofer <meschti@gmail.com>
2016-07-13 20:07:15 +0200

    Added a project for the solution space visualization

---
+++ solution_space_visualization/Program.cs
@@ -0,0 +1,20 @@
+﻿using at.mschwaig.mped.definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solution_space_visualization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s1 = new Solution(new int[] { 0, 1, 2, 3, 4 });
+            Solution s2 = new Solution(new int[] { 1, 0, 3, 2, 4 });
+
+            var dist = s1.getDistanceTo(s2);
+        }
+    }
+}
