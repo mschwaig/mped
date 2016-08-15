53b07f6c99397041369e11d8fb1d0fff283945f0
Martin Schwaighofer <meschti@gmail.com>
2016-08-15 21:28:30 +0200

    added missing constructor implementation for HeuristicRun

--- definitions/HeuristicRun.cs
+++ definitions/HeuristicRun.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 
 namespace at.mschwaig.mped.definitions
 {
+    [Table("HEURISTIC_RUN")]
     public class HeuristicRun
     {
         public enum AlgorithmType
@@ -19,10 +21,15 @@ namespace at.mschwaig.mped.definitions
         public AlgorithmType Algorithm { get; private set; }
         public DateTime StartTime { get; private set; }
         public string Parameters { get; private set; }
-        public string gitHash { get; private set; }
-        public bool uncommitted_changes { get; private set; }
+        public string GitHash { get; private set; }
+        public bool UncommittedChanges { get; private set; }
 
         public HeuristicRun(AlgorithmType algorithm, DateTime startTime, string parameters, string git_hash, bool uncommitted_changes) {
+            this.Algorithm = algorithm;
+            this.StartTime = startTime;
+            this.Parameters = parameters;
+            this.GitHash = git_hash;
+            this.UncommittedChanges = uncommitted_changes;
         }
     }
 }
