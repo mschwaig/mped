ddca1f89b11e920db8a131790d41c83d9ff44ec8
Martin Schwaighofer <meschti@gmail.com>
2016-12-13 14:13:14 +0100

    Removed unused experimental setups from problemgen project

--- problemgen/Program.cs
+++ problemgen/Program.cs
@@ -8,52 +8,6 @@ namespace at.mschwaig.mped.problemgen
 {
     class Program
     {
-        static void generateExhaustiveComparisonExperiment()
-        {
-            Experiment experiment = new Experiment("ExhaustiveComparison");
-
-            List<Problem> problems = new List<Problem>();
-            RandomNumberGenerator r = new RNGCryptoServiceProvider();
-
-            int runId = r.Next<RandomNumberGenerator>(int.MaxValue);
-
-            int[] alphabet_sizes = { 4, 6, 8, 10, 12, 16, 24, 32, 42 };
-            int[] string_lengths = { 256 };
-            double[] edit_probabilities = { .0d, .2d, .4d, .6d, .8d, 1.0d };
-            double[] insert_probabilities = { .0d, .2d, .4d, .6d, .8d };
-            double[] delete_probabilities = { .0d, .2d, .4d, .6d, .8d };
-
-            foreach (var alphabet_size in alphabet_sizes)
-            foreach (var string_length in string_lengths)
-            foreach (var edit_probability in edit_probabilities)
-            foreach (var insert_probability in insert_probabilities)
-            foreach (var delete_probability in delete_probabilities)
-            {
-                if (!(edit_probability + insert_probability + delete_probability > 1.0d))
-                {
-                    problems.Add(Problem.generateProblem(experiment, alphabet_size, string_length, edit_probability, insert_probability, delete_probability, LengthCorrectionPolicy.NO_CORRECTION, r, runId));
-                }
-                if (!(edit_probability + insert_probability + delete_probability > 1.0d) && insert_probability <= .5d && delete_probability <= .5d)
-                {
-                    problems.Add(Problem.generateProblem(experiment, alphabet_size, string_length, edit_probability, insert_probability, delete_probability, LengthCorrectionPolicy.PREPEND_CORRECTION, r, runId));
-                    problems.Add(Problem.generateProblem(experiment, alphabet_size, string_length, edit_probability, insert_probability, delete_probability, LengthCorrectionPolicy.APPEND_CORRECTION, r, runId));
-                }
-                if (!(edit_probability > 1.0d) && delete_probability == .0d && insert_probability == .0d)
-                {
-                    // NOTE: equally distributed insertions and deletions cancel each other out
-                    problems.Add(Problem.generateProblem(experiment, alphabet_size, string_length, edit_probability, insert_probability, insert_probability, LengthCorrectionPolicy.DISTRIBUTE_CORRECTION, r, runId));
-                }
-            }
-
-
-            using (var ctx = new ThesisDbContext())
-            {
-                ctx.Problems.AddRange(problems);
-                ctx.Experiments.Add(experiment);
-                ctx.SaveChanges();
-            }
-        }
-
         static void generateInsertExperiment()
         {
             Experiment experiment = new Experiment("Insert");
@@ -97,36 +51,10 @@ namespace at.mschwaig.mped.problemgen
         }
 
 
-        static void generateSizeScalingExperiment()
+
+        static void generateCompareExperiment(string name, int repetitions)
         {
-            Experiment experiment = new Experiment("SizeScaling");
-
-            List<Problem> problems = new List<Problem>();
-            RandomNumberGenerator r = new RNGCryptoServiceProvider();
-
-            int runId = r.Next<RandomNumberGenerator>(int.MaxValue);
-
-            int[] alphabet_sizes = { 4, 6, 8, 10, 12, 16, 24, 32, 42 };
-
-            foreach (var alphabet_size in alphabet_sizes)
-            {
-                problems.Add(Problem.generateProblem(experiment, alphabet_size, 256, 1.0d, .0d, .0d, LengthCorrectionPolicy.NO_CORRECTION, r, runId));
-            }
-
-
-
-
-            using (var ctx = new ThesisDbContext())
-            {
-                ctx.Problems.AddRange(problems);
-                ctx.Experiments.Add(experiment);
-                ctx.SaveChanges();
-            }
-        }
-
-        static void generateCompareExperiment()
-        {
-            Experiment experiment = new Experiment("Compare");
+            Experiment experiment = new Experiment(name);
 
             List<Problem> problems = new List<Problem>();
             RandomNumberGenerator r = new RNGCryptoServiceProvider();
@@ -137,12 +65,17 @@ namespace at.mschwaig.mped.problemgen
 
             foreach (var alphabet_size in alphabet_sizes)
             {
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < repetitions; i++)
                 {
                     problems.Add(Problem.generateProblem(experiment, alphabet_size, 256, 1.0d, .0d, .0d, LengthCorrectionPolicy.NO_CORRECTION, r, runId));
                 }
 
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < repetitions; i++)
+                {
+                    problems.Add(Problem.generateProblem(experiment, alphabet_size, 256, 0.5d, .0d, .0d, LengthCorrectionPolicy.NO_CORRECTION, r, runId));
+                }
+
+                for (int i = 0; i < repetitions; i++)
                 {
                     problems.Add(Problem.generateProblem(experiment, alphabet_size, 256, 0.0d, .0d, .0d, LengthCorrectionPolicy.NO_CORRECTION, r, runId));
                 }
@@ -159,13 +92,16 @@ namespace at.mschwaig.mped.problemgen
             }
         }
 
+        static void generateFullComparisonExperiment()
+        {
+            generateCompareExperiment("FullComparison", 1);
+        }
+
 
         static void Main(string[] args)
         {
-            // createExhaustiveComparisonExperiment()
             generateInsertExperiment();
-            // generateSizeScalingExperiment();
-            // generateCompareExperiment();
+            generateFullComparisonExperiment();
         }
     }
 }
